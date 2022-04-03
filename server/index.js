fb0e4779741eff70f43ac01d7ccefa87077189a5
Gustav Palm <gustav.palm@grebban.com>
2022-04-03 19:07:53 +0200

    ADD nginx backbone

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-
-app.use(cors())
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
